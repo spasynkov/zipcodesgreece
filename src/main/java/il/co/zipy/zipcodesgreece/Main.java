713cd3a071ec42bfbb47a181684af6fc40a35f06
Stanislav Pasynkov <s.pasynkov@gmail.com>
2018-07-31 18:31:36 +0300

    lots of changes made, lol

--- src/main/java/il/co/zipy/zipcodesgreece/Main.java
+++ src/main/java/il/co/zipy/zipcodesgreece/Main.java
@@ -8,19 +8,17 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.List;
+import java.io.*;
+import java.util.*;
 import java.util.stream.Collectors;
 
-public class Main {
-	private final WebDriver driver;
-	private final WebDriverWait wait;
-	private boolean tableUpdated = false;
+public class Main implements Serializable {
+	private transient final WebDriver driver;
+	private transient final WebDriverWait wait;
+	private transient boolean tableUpdated = false;
+
+	private static final String CITIES_URLS = "citiesUrls.ser";
+	private static final String ADDRESSES = "address.ser";
 
 	public Main() throws FileNotFoundException {
 		String chromeDriverPath = "resources/chromedriver.exe";
@@ -29,7 +27,7 @@ public class Main {
 		System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
 
 		driver = new ChromeDriver();
-		wait = new WebDriverWait(driver, 10);
+		wait = new WebDriverWait(driver, 20);
 	}
 
 	private File getResourceFile(String path) throws FileNotFoundException {
@@ -47,25 +45,41 @@ public class Main {
 	}
 
 	private void run() {
-		List<ZipCodeDataHolder> result = new LinkedList<>();
+		Map<String, Set<ZipCodeDataHolder>> pageUrlWithZipCodes = new HashMap<>();  // <url, zipCodes>
 
-		// collecting data
-		List<String> perfecturesUrls = getLinksInDivByClassName("https://www.xo.gr/dir-tk/?lang=en", "perfecturesearches");
-		closeCookiesInfoMessage();
-		List<String> citiesUrl;
-		for (String perfectureUrl : perfecturesUrls) {
-			citiesUrl = getLinksInDivByClassName(perfectureUrl, "moreCities_content");
-			for (String cityUrl : citiesUrl) {
-				driver.get(cityUrl);
-				List<ZipCodeDataHolder> dataFromTable = parsePage();
-				result.addAll(dataFromTable);
-			}
-			doRandomWait();
+		List<String> citiesUrl = null;
+		File file = new File(CITIES_URLS);
+		if (file.exists() && file.isFile()) {
+			citiesUrl = deserialize(file.getAbsolutePath());
+			System.out.println("Loaded " + citiesUrl.size() + " cities urls from file " + CITIES_URLS);
+		}
+
+		if (citiesUrl == null || citiesUrl.isEmpty()) {
+			citiesUrl = collectCities();
+		}
+
+		Collections.shuffle(citiesUrl);
+
+		boolean isFinished = false;
+		try {
+			collectZipCodes(citiesUrl, pageUrlWithZipCodes);
+			isFinished = true;
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("Saving " + pageUrlWithZipCodes.size() + " entries");
+			serialize(pageUrlWithZipCodes, ADDRESSES);  // saving data to avoid getting it later
 		}
 
 		driver.quit();
 
+		if (!isFinished) return;
+
 		// saving data
+		final List<ZipCodeDataHolder> result = new LinkedList<>();
+		pageUrlWithZipCodes.forEach((k, v) -> {
+			result.addAll(v);
+		});
+
 		try (FileWriter fw = new FileWriter("greekzipcodes.txt", true)) {
 			result.forEach(c -> {
 				try {
@@ -79,6 +93,94 @@ public class Main {
 		}
 	}
 
+	private void collectZipCodes(List<String> citiesUrl, Map<String, Set<ZipCodeDataHolder>> result) {
+		// loading previous data
+		if (result.isEmpty()) {
+			File file = new File(ADDRESSES);
+			if (file.exists() && file.isFile()) {
+				Map<String, Set<ZipCodeDataHolder>> previousData = deserialize(file.getAbsolutePath());
+				if (previousData != null) {
+					String key;
+					Set<ZipCodeDataHolder> value;
+					for (Map.Entry<String, Set<ZipCodeDataHolder>> pair : previousData.entrySet()) {
+						key = pair.getKey();
+						value = pair.getValue();
+						citiesUrl.remove(key);
+						result.put(key, value);
+					}
+					System.out.println("Loaded " + result.size() + " entries from file " + ADDRESSES);
+				}
+			}
+		}
+
+		// collecting new data
+		for (String cityUrl : citiesUrl) {
+			driver.get(cityUrl);
+			Set<ZipCodeDataHolder> dataFromTable = parsePage();
+			result.put(cityUrl, dataFromTable);
+		}
+
+		// saving
+		serialize(result, ADDRESSES);
+	}
+
+	private <T> void serialize(T object, String filename) {
+		try {
+			FileOutputStream fout = new FileOutputStream(filename);
+			ObjectOutputStream oos = new ObjectOutputStream(fout);
+			oos.writeObject(object);
+		} catch (Exception e) {
+			System.err.println("Error while serialising object");
+			e.printStackTrace();
+		}
+	}
+
+	private <T> T deserialize(String filename) {
+		try {
+			FileInputStream fileInputStream = new FileInputStream(filename);
+			ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
+			Object o = objectInputStream.readObject();
+			return (T) o;
+		} catch (Exception e) {
+			System.err.println("Failed to read object");
+		}
+		return null;
+	}
+
+	private List<String> collectCities() {
+		// collecting data
+		List<String> perfecturesUrls = getLinksInDivByClassName("https://www.xo.gr/dir-tk/?lang=en", "perfecturesearches");
+		Collections.shuffle(perfecturesUrls);
+		closeCookiesInfoMessage();
+		List<String> citiesUrl = new LinkedList<>();
+		for (String perfectureUrl : perfecturesUrls) {
+			Set<String> citiesPagesUrls = collectCitiesPages(perfectureUrl);
+			for (String cityPage : citiesPagesUrls) {
+				avoidRobotAlert();
+				citiesUrl.addAll(getLinksInDivByClassName(cityPage, "moreCities_content"));
+			}
+
+		}
+
+		System.out.println("Collected " + citiesUrl.size() + " cities url");
+
+		serialize(citiesUrl, CITIES_URLS);
+		return citiesUrl;
+	}
+
+	private Set<String> collectCitiesPages(String perfectureUrl) {
+		Set<String> result = new HashSet<>();
+		result.add(perfectureUrl);
+		driver.get(perfectureUrl);
+		try {
+			for(WebElement e : driver.findElements(By.cssSelector("div[class=\"row pagination\"] a"))) {
+				result.add(e.getAttribute("href"));
+			}
+		} catch (Exception ignored) {}
+
+		return result;
+	}
+
 	private List<String> getLinksInDivByClassName(String url, String className) {
 		System.out.println("Going to page " + url);
 		driver.get(url);
@@ -86,26 +188,20 @@ public class Main {
 				.stream().map(x -> x.getAttribute("href")).collect(Collectors.toList());
 	}
 
-	private List<ZipCodeDataHolder> parsePage() {
-		List<ZipCodeDataHolder> result = new LinkedList<>();
+	private Set<ZipCodeDataHolder> parsePage() {
+		Set<ZipCodeDataHolder> result = new HashSet<>();
 		WebElement table;
 		By tableLocator = By.id("zipTable");
 		do {
-			if (driver.getPageSource().contains("Are you a robot!")) {
-				System.err.println("Robot alert occurs. Data could be doubled probably");
-				try {
-					Thread.sleep(2000);
-				} catch (InterruptedException ignored) {}
-				driver.navigate().refresh();
-			}
+			avoidRobotAlert();
 
 			if (tableUpdated) {
-				wait.until(input -> ((JavascriptExecutor) input).executeScript("return document.readyState").equals("complete"));
 				wait.until(ExpectedConditions.visibilityOfElementLocated(tableLocator));
 				wait.until(ExpectedConditions.elementToBeClickable(tableLocator));
 				try {
 					Thread.sleep(5000);
-				} catch (InterruptedException ignored) {}
+				} catch (InterruptedException ignored) {
+				}
 			}
 
 			table = driver.findElement(tableLocator);
@@ -113,6 +209,15 @@ public class Main {
 		} while (clickNext());
 		System.out.println("Got " + result.size() + " element(s) in total from page\t" + driver.getCurrentUrl());
 		return result;
+	}
+
+	private void avoidRobotAlert() {
+		wait.until(input -> ((JavascriptExecutor) input).executeScript("return document.readyState").equals("complete"));
+		if (driver.getPageSource().contains("Are you a robot!")) {
+			System.err.println("Robot alert occurs. Data could be doubled probably");
+			doRandomWait(2000);
+			driver.navigate().refresh();
+		}
 	}
 
 	private List<ZipCodeDataHolder> parseTable(List<WebElement> table) {
@@ -132,19 +237,25 @@ public class Main {
 	}
 
 	private void doRandomWait() {
-		int fromInSeconds = 2;
+		doRandomWait(0);
+	}
+
+
+	private void doRandomWait(long basicLatencyMillis) {
+		int fromInSeconds = 3;
 		int toInSeconds = 10;
 
-		int wait = (int) (fromInSeconds * 1000 + Math.random() * ((toInSeconds - fromInSeconds) * 1000));
+		long wait = (int) (fromInSeconds * 1000 + Math.random() * ((toInSeconds - fromInSeconds) * 1000)) + basicLatencyMillis;
 		try {
 			Thread.sleep(wait);
-		} catch (InterruptedException ignored) { }
+		} catch (InterruptedException ignored) {
+		}
 	}
 
 	private boolean clickNext() {
 		tableUpdated = false;
 		try {
-			//closeCookiesInfoMessage();
+			closeCookiesInfoMessage();
 			List<WebElement> pages = driver.findElement(By.id("pager")).findElements(By.tagName("li"));
 			WebElement link = findNextPage(pages).findElement(By.tagName("a"));
 			//wait.until(ExpectedConditions.elementToBeClickable(link));
@@ -165,7 +276,8 @@ public class Main {
 		try {
 			driver.findElement(By.cssSelector("a[data-cc-event=\"click:dismiss\"]")).click();
 			System.out.println("Cookies warning is closed");
-		} catch (Exception e) {}
+		} catch (Exception e) {
+		}
 	}
 
 	private WebElement findNextPage(List<WebElement> pages) {
@@ -180,7 +292,7 @@ public class Main {
 		return null;
 	}
 
-	private class ZipCodeDataHolder {
+	private class ZipCodeDataHolder implements Serializable {
 		private String prefecture;
 		private String city;
 		private String streetAndNumber;
@@ -197,6 +309,22 @@ public class Main {
 		@Override
 		public String toString() {
 			return this.prefecture + ";" + this.city + ";" + this.streetAndNumber + ";" + this.postalCode + "\n";
+		}
+
+		@Override
+		public boolean equals(Object o) {
+			if (this == o) return true;
+			if (o == null || getClass() != o.getClass()) return false;
+			ZipCodeDataHolder that = (ZipCodeDataHolder) o;
+			return Objects.equals(prefecture, that.prefecture) &&
+					Objects.equals(city, that.city) &&
+					Objects.equals(streetAndNumber, that.streetAndNumber) &&
+					Objects.equals(postalCode, that.postalCode);
+		}
+
+		@Override
+		public int hashCode() {
+			return Objects.hash(prefecture, city, streetAndNumber, postalCode);
 		}
 	}
 }
